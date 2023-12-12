618ded76b86c9a66d4bb4478175a74bf064ca7fb
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2023-12-12 19:59:22 +0100

    Create project proposition view and route to view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Needs to go through project controller (create)
+Route::get('/project/create', function() {
+    return view('shared.project_proposition');
+});
