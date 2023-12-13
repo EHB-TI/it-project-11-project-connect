5b60b50a58664a2581495a5f0d3bfc9979646587
Lucaswillaert <lucas.willaert@student.ehb.be>
2023-12-13 21:53:15 +0100

    Add create and store methods to SpaceController

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,16 @@ Route::get('/project/create', function() {
 
 Route::get('/space', function () {
     return view('shared.space');
-});
+}) -> name('space');
 
+// Needs to go through space controller (create)
+Route::get('/space/create', function () {
+    return view('shared.space_create');
+})->name('spaces.create');
 
+Route::get('/space', 'App\Http\Controllers\SpaceController@index');
+
+Route::post('/space', 'App\Http\Controllers\SpaceController@store')->name('space.store');
 
 // Needs to go through project controller (create)
 Route::get('/project/create', function() {
