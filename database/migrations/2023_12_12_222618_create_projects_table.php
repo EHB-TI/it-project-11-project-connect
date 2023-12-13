b4bacf6b7ec75a6ffa1c61bb8461ae78c94bdf26
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-13 01:37:37 +0100

    update: project tablle migrations

--- database/migrations/2023_12_12_222618_create_projects_table.php
+++ database/migrations/2023_12_12_222618_create_projects_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 100)->unique();
+            $table->string('description', 255)->nullable();
+            $table->string('status', 255)->nullable();
+
+            $table->unsignedBigInteger('ownerID')->nullable();
+            /**$table->unsignedBigInteger('spaceID')->nullable(); */
+
+            $table->foreign('ownerID')->references('id')->on('users');
+            /**$table->foreign('spaceID')->references('id')->on('spaces'); */
         });
     }
 
