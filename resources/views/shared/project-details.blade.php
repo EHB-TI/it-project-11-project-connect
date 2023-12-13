4cc7fff9ed7d43e2b9ed4e3075520ad9a923b2fd
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-13 14:16:37 +0100

    Add: create project overview page

---
+++ resources/views/shared/project-details.blade.php
@@ -0,0 +1,13 @@
+@extends('components.head')
+@section('title', 'Home')
+
+@section('content')
+    <div class="flex">
+        <div class="w-2/3">
+            <h1>test</h1>
+        </div>
+        <div class="w-1/3">
+            <h1>test</h1>
+        </div>
+    </div>
+@endsection
