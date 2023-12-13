f44a150a3c1da538bb33b63be0c8c010028ed32f
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-13 15:48:02 +0100

    Add: Create project controller

---
+++ app/Http/Controllers/projectController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class projectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $project = Project::find($id);
+
+        if ($project === null) {
+            // Redirect back or show an error message
+            return redirect('/')->with('error', 'Project not found');
+        }
+
+        return view('shared.project-details', ['project' => $project]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
