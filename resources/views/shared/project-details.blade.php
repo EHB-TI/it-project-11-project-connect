e388f934ef465a945283fa2a459c54d494ea5354
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-13 15:51:19 +0100

    Add: Parse project description markdown

--- resources/views/shared/project-details.blade.php
+++ resources/views/shared/project-details.blade.php
@@ -4,10 +4,15 @@
 @section('content')
     <div class="flex">
         <div class="w-2/3">
-            <h1>test</h1>
+            <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">{{ $project->name }}</h1>
         </div>
-        <div class="w-1/3">
-            <h1>test</h1>
+        <div class="w-1/3 pl-5">
+            @php
+                $parsedown = new Parsedown();
+            @endphp
+            <div id="details-content">
+                {!! $parsedown->text($project->description) !!}
+            </div>
         </div>
     </div>
 @endsection
