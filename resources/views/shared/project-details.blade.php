8b3147058b6086e68b672fb78c3ff436b636772e
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-14 14:04:13 +0100

    fix: visualise active section in side nav

--- resources/views/shared/project-details.blade.php
+++ resources/views/shared/project-details.blade.php
@@ -1,5 +1,5 @@
 @extends('components.head')
-@section('title', 'Home')
+@section('title', 'Project Details - ' . $project->name)
 
 @section('content')
     <div class="flex">
@@ -10,7 +10,7 @@
             </div>
         </div>
         <div class="w-1/3 pl-5">
-            <ul class="rounded-xl border-2">
+            <ul class="rounded-xl border-2 overflow-hidden">
                 <li class="border-b-2"><button id="overview" class="projectDetails__button w-full p-2">Overview</button></li>
                 <li class="border-b-2"><button id="feedback" class="projectDetails__button w-full p-2">Feedback</button></li>
                 <li class="border-b-2"><button id="members" class="projectDetails__button w-full p-2">Members</button></li>
@@ -19,23 +19,30 @@
         </div>
     </div>
     <script>
+        let activeBgColor = 'bg-gray-700';
+        let activeTextColor = 'text-white';
         document.addEventListener('DOMContentLoaded', function() {
             let projectId = {{ $project->id }};
             fetch('/project/details/overview/' + projectId)
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('details-content').innerHTML = data;
+                    document.getElementById('overview').parentNode.classList.add(activeBgColor, activeTextColor);
                 });
         });
 
         document.querySelectorAll('button').forEach(button => {
             button.addEventListener('click', function() {
                 let section = this.getAttribute('id');
-                let projectId = {{ $project->id }}
+                let projectId = {{ $project->id }};
                 fetch('/project/details/' + section + '/' + projectId)
                     .then(response => response.text())
                     .then(data => {
                         document.getElementById('details-content').innerHTML = data;
+                        document.querySelectorAll('li').forEach(li => {
+                            li.classList.remove(activeBgColor, activeTextColor);
+                        });
+                        this.parentNode.classList.add(activeBgColor, activeTextColor);
                     });
             });
         });
