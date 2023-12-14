ca36f782261e95d3f6b9e42f03354705e86c315a
chris09877 <104131775+chris09877@users.noreply.github.com>
2023-12-14 18:45:44 +0100

    little fixes

--- resources/views/students/approvedProjects.blade.php
+++ resources/views/students/approvedProjects.blade.php
@@ -6,12 +6,13 @@
     <div class="app-content ml-[max(250px,_20%)] p-5">
 
         <h1>All projects</h1>
-        @if($projects->isEmpty())
+        {{-- @if($projects->count()) --}}
     <p>Nothing here yet, come back later or start your own project.</p>
     <hr>
-@else
-<a href="{{ route('', $project->id) }}">
+{{-- @else --}}
     @foreach ($projects as $project)
+    <a href="{{ route('project.details', $project->id) }}">
+
         <img src="{{$project->filepath}}" alt="project image">
         <p>{{$project->name}}</p>
         @if ($project->owner)
@@ -20,9 +21,10 @@
             <p>Created By: nobody</p>
         @endif
         <p>{{$project->description}}</p>
+    </a>
+
     @endforeach
-</a>
-@endif
+{{-- @endif --}}
 
         <h1>That's it <br>Found nothing that appeals to you? <br>Becpme a Product Owner.</h1>
         <p>Do you have a brilliant idea waiting to come to life?
