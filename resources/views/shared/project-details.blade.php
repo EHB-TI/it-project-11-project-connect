876f4782cda92316704bf36205ee4a4b2bc38027
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-14 16:52:39 +0100

    Add: Apply card

--- resources/views/shared/project-details.blade.php
+++ resources/views/shared/project-details.blade.php
@@ -2,22 +2,28 @@
 @section('title', 'Project Details - ' . $project->name)
 
 @section('content')
-    <div class="flex">
-        <div class="w-2/3">
-            <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">{{ $project->name }}</h1>
+    <div class="flex gap-8">
+        <div class="w-3/4">
+            <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">{{ $project->name }}</h1>
             <div id="details-content">
 
             </div>
         </div>
-        <div class="w-1/3 pl-5">
+        <div class="w-1/4">
+            <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+                <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Apply for this project</h1>
+                <p class="mb-4">Want to be part of this project?</p>
+                <button class="project-detail__applyButton rounded-full px-4 py-2 border-2">Apply now</button>
+            </div>
             <ul class="rounded-xl border-2 overflow-hidden">
-                <li class="border-b-2"><button id="overview" class="projectDetails__button w-full p-2">Overview</button></li>
-                <li class="border-b-2"><button id="feedback" class="projectDetails__button w-full p-2">Feedback</button></li>
-                <li class="border-b-2"><button id="members" class="projectDetails__button w-full p-2">Members</button></li>
-                <li class=""><button id="applications" class="projectDetails__button w-full p-2">Applications</button></li>
+                <li class="border-b-2"><button id="overview" class="projectDetails__navButton w-full p-2">Overview</button></li>
+                <li class="border-b-2"><button id="feedback" class="projectDetails__navButton w-full p-2">Feedback</button></li>
+                <li class="border-b-2"><button id="members" class="projectDetails__navButton w-full p-2">Members</button></li>
+                <li class=""><button id="applications" class="projectDetails__navButton w-full p-2">Applications</button></li>
             </ul>
         </div>
     </div>
+
     <script>
         let activeBgColor = 'bg-gray-700';
         let activeTextColor = 'text-white';
@@ -31,7 +37,7 @@
                 });
         });
 
-        document.querySelectorAll('button').forEach(button => {
+        document.querySelectorAll('.projectDetails__navButton').forEach(button => {
             button.addEventListener('click', function() {
                 let section = this.getAttribute('id');
                 let projectId = {{ $project->id }};
