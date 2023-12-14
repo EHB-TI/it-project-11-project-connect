b40307c079f163f50efe6e48ba1639bf2195a752
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-14 13:52:16 +0100

    Add: Feedback card layout on feedback detail page

--- resources/views/shared/detail-sections/feedback.blade.php
+++ resources/views/shared/detail-sections/feedback.blade.php
@@ -1,1 +1,60 @@
-<h1>test Feedback</h1>
+<div class="feedback-card rounded-xl border-2 my-8">
+    <div class="feedback-card__header flex justify-between border-b-2 p-4">
+        <div class="user-info flex items-center gap-2">
+            <div class="user-info__avatar w-10 h-10 rounded-full bg-indigo-950"></div>
+            <div class="user-info__name h-fit">Ruben dejonckheere</div>
+        </div>
+        <div class="date-info flex gap-2 items-center">
+            <div class="date-info__date h-fit">14/12/2021</div>
+            <div class="date-info__time h-fit">12:12</div>
+        </div>
+    </div>
+    <div class="feedback-card__body p-4">
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+    </div>
+</div>
+<div class="feedback-card rounded-xl border-2 my-8">
+    <div class="feedback-card__header flex justify-between border-b-2 p-4">
+        <div class="user-info flex items-center gap-2">
+            <div class="user-info__avatar w-10 h-10 rounded-full bg-indigo-950"></div>
+            <div class="user-info__name h-fit">Ruben dejonckheere</div>
+        </div>
+        <div class="date-info flex gap-2 items-center">
+            <div class="date-info__date h-fit">11/12/2021</div>
+            <div class="date-info__time h-fit">16:43</div>
+        </div>
+    </div>
+    <div class="feedback-card__body p-4">
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+    </div>
+</div>
+<div class="feedback-card rounded-xl border-2 my-8">
+    <div class="feedback-card__header flex justify-between border-b-2 p-4">
+        <div class="user-info flex items-center gap-2">
+            <div class="user-info__avatar w-10 h-10 rounded-full bg-indigo-950"></div>
+            <div class="user-info__name h-fit">Ruben dejonckheere</div>
+        </div>
+        <div class="date-info flex gap-2 items-center">
+            <div class="date-info__date h-fit">8/12/2021</div>
+            <div class="date-info__time h-fit">8:51</div>
+        </div>
+    </div>
+    <div class="feedback-card__body p-4">
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+    </div>
+</div>
