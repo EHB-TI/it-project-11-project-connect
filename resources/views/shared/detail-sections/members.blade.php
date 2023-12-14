41807f17dfc7add4cb90c1f195d87cf44fdd57be
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-14 12:13:03 +0100

    Add: Created detail-sections for students

---
+++ resources/views/shared/detail-sections/members.blade.php
@@ -0,0 +1,1 @@
+<h1>test Members</h1>
--- resources/views/shared/project-details.blade.php
+++ resources/views/shared/project-details.blade.php
@@ -5,14 +5,31 @@
     <div class="flex">
         <div class="w-2/3">
             <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">{{ $project->name }}</h1>
-        </div>
-        <div class="w-1/3 pl-5">
-            @php
-                $parsedown = new Parsedown();
-            @endphp
             <div id="details-content">
-                {!! $parsedown->text($project->description) !!}
+
             </div>
         </div>
+        <div class="w-1/3 pl-5">
+            <ul class="rounded border-2">
+                <li class="border-b-2"><button id="overview" class="projectDetails__button w-full p-2">Overview</button></li>
+                <li class="border-b-2"><button id="feedback" class="projectDetails__button w-full p-2">Feedback</button></li>
+                <li class="border-b-2"><button id="members" class="projectDetails__button w-full p-2">Members</button></li>
+                <li class=""><button id="applications" class="projectDetails__button w-full p-2">Applications</button></li>
+            </ul>
+        </div>
     </div>
+    <script>
+        document.querySelectorAll('button').forEach(button => {
+            button.addEventListener('click', function() {
+                let section = this.getAttribute('id');
+                let projectId = {{ $project->id }}
+                fetch('/project/details/' + section + '/' + projectId)
+                    .then(response => response.text())
+                    .then(data => {
+                        document.getElementById('details-content').innerHTML = data;
+                    });
+            });
+        });
+    </script>
 @endsection
+
