c90f2fad237b432184cb6b1e733be467000e1990
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-18 06:23:35 +0100

    Add: Subtitle to pages

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,10 +1,10 @@
 @extends('components.head')
 @section('title', 'Home')
 @section('content')
-
+<h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">Create project</h2>
 <div class="flex flex-col gap-2">
     <div class="w-1/2">
-        <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Step 1: Project Information</h1>
+        <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Project Information</h1>
         <p class="mb-4">Describe why your idea will help fulfill an existing need. Make sure and explain where there is room for
             new technologies to experiment with (not seen during your previous study). Tell us the reason why you
             want to pitch your own idea.
@@ -23,7 +23,7 @@
             </fieldset>
             <div class="flex flex-col space-y-2">
                 <input type="hidden" id="content" name="description" class="hidden">
-                <label for="editor" class="text-gray-600 font-semibold">Write your motivation down below</label>
+                <label for="editor" class="text-gray-600 font-semibold">Write a detailed description down below</label>
                 <div id="editor" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm"></div>
             </div>
 
