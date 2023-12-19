24209d8f141ff22b45b9f67d0e38306ab27cb6be
Lucaswillaert <lucas.willaert@student.ehb.be>
2023-12-19 16:23:21 +0100

    space_id bugfix

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -22,8 +22,10 @@ class ProjectController extends Controller
     public function index()
     {
         $space_id = session('current_space_id');
+        $space = Space::find($space_id);
         //authenticatie teacher for all projects
         if (Auth::user()->role == 'teacher'){
+            session('space_id');
             $projects = Space::find($space_id)->projects()->get();
         }else{
             $projects = Space::find($space_id)->projects()->where('status', 'published')->get();
