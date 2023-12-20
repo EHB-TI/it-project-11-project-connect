00a3340a2888a2405992a0f118428b23c8dd6341
chris09877 <104131775+chris09877@users.noreply.github.com>
2023-12-20 14:54:29 +0100

    fix missing div

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,28 +2,31 @@
 @section('title', 'Home')
 @section('content')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
-    <div class="flex gap-8">
+<div class="flex gap-8">
 
-        <div class="w-1/4">
-            <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Next Deadline</h1>
-                <p class="mb-4">Potential product owner: Pitch your creative concept</p>
-                <p class="mb-4"> <strong>Who?</strong> </p>
-                <p class="mb-4"><strong>When?</strong></p>
-                {{-- <p class="mb-4">hier komt deadline</p> --}}
+    <div class="w-1/4">
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
+                Next Deadline</h1>
+            <p class="mb-4">Potential product owner: Pitch your creative concept</p>
+            <p class="mb-4"> <strong>Who?</strong> </p>
+            <p class="mb-4"><strong>When?</strong></p>
+            {{-- <p class="mb-4">hier komt deadline</p> --}}
 
-            </div>
-            
         </div>
 
+    </div>
 
+    <div>
         <canvas id="pieChart" width="400" height="400"></canvas>
         <canvas id="barChart" width="400" height="400"></canvas>
 
+    </div>
 
 
-        <script>
-             // Pie Chart
+
+    <script>
+        // Pie Chart
         let labels = ['Product Owner', 'Applicants', 'Inactive Students'];
         let itemData = [{{$po}}, {{$applicants}}, {{$inactiveStudents}}];
 
@@ -63,13 +66,16 @@
         const barLabels = ['approved', 'denied', 'closed', 'pending', 'published'];
         const barData = {
             labels: barLabels,
+            
             datasets: [{
                 label: 'approved',
             data: [{{$approvedProjects}}],
             backgroundColor: 'rgb(75, 192, 192)',
             borderColor: 'rgb(75, 192, 192)',
             borderWidth: 1
-        }, {
+        }
+        , 
+        {
             label: 'denied',
             data: [{{$deniedProjects}}],
             backgroundColor: 'rgb(255, 99, 132)',
@@ -126,171 +132,77 @@
             document.getElementById('barChart'),
             barConfig
         );
-            // document.ready(()=>{
-            //     let labels = ['Product Owner', 'Applicants', 'Inactive Students'];
-            //     let itemData = [{{$po}}, {{$applicants}}, {{$inactiveStudents}}];
-
-            //         const DATA_COUNT = 3;
-            //         const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: {{$spaceUsers}}};
-
-            //         const data = {
-            //         labels: labels,
-            //         datasets: itemData
-            //         //[
-            //         //     {
-            //         //     label: 'Dataset 1',
-            //         //     data: Utils.numbers(NUMBER_CFG),
-            //         //     backgroundColor: Object.values(Utils.CHART_COLORS),
-            //         //     }
-            //         // ]
-            //         };
-
-
-            //         const config = {
-            //                             type: 'pie',
-            //                             data: itemData,
-            //                             options: {
-            //                                 responsive: true,
-            //                                 plugins: {
-            //                                 legend: {
-            //                                     position: 'top',
-            //                                 },
-            //                                 title: {
-            //                                     display: true,
-            //                                     text: 'Students'
-            //                                 }
-            //                                 }
-            //                             },
-            //                         };
-            //         const chart = new Chart(
-            //             document.getElementById('pieChart'), config
-            //             )
-            //         });
-                    
-            //         //BAR CHART
-            //         const DATA_COUNT = 5;
-            //         const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: {{$allProjects}}};
-
-            //         const labels = ['approved', 'decline', 'reject', 'pending', 'published'];
-            //         const data = {
-            //         labels: labels,
-            //         datasets: [
-            //             {
-            //             label: 'approved',
-            //             data: Utils.numbers(NUMBER_CFG),
-            //             borderColor: Utils.CHART_COLORS.red,
-            //             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-            //             borderWidth: 2,
-            //             borderRadius: Number.MAX_VALUE,
-            //             borderSkipped: false,
-            //             },
-            //             {
-            //             label: 'declines',
-            //             data: Utils.numbers(NUMBER_CFG),
-            //             borderColor: Utils.CHART_COLORS.blue,
-            //             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-            //             borderWidth: 2,
-            //             borderRadius: 5,
-            //             borderSkipped: false,
-            //             },
-            //             {
-            //             label: 'reject',
-            //             data: Utils.numbers(NUMBER_CFG),
-            //             borderColor: Utils.CHART_COLORS.blue,
-            //             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-            //             borderWidth: 2,
-            //             borderRadius: 5,
-            //             borderSkipped: false,
-            //             },
-            //             {
-            //             label: 'pending',
-            //             data: Utils.numbers(NUMBER_CFG),
-            //             borderColor: Utils.CHART_COLORS.blue,
-            //             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-            //             borderWidth: 2,
-            //             borderRadius: 5,
-            //             borderSkipped: false,
-            //             },
-            //             {
-            //             label: 'published',
-            //             data: Utils.numbers(NUMBER_CFG),
-            //             borderColor: Utils.CHART_COLORS.blue,
-            //             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-            //             borderWidth: 2,
-            //             borderRadius: 5,
-            //             borderSkipped: false,
-            //             }
-            //         ]
-            //         const chart = new Chart(
-            //     document.getElementById('barChart'), config
-            //     )
-            // });
         
-        </script>
+    </script>
 
     {{-- <div class="w-3/4">
         @if ($projects->count()===0)
-        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Projects</h1>
-            <hr>
-            <p class="mb-4">There are not yet any project proposals. <br></p>
-            <br>
-            <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Projects</h1>
+        <hr>
+        <p class="mb-4">There are not yet any project proposals. <br></p>
+        <br>
+        <br>
         @else
-        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Pending Projects</h1>
-            @foreach ($projects as $project)
-                        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                            <img src="{{$project->filepath}}" alt="project image">
-                            <p class="mb-4">{{$project->name}}</p>
-                            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
-                        </div>
-            @endforeach
-            <br>
-            <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Published Projects</h1>
-            @foreach ($projects as $project)
-                        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                            <img src="{{$project->filepath}}" alt="project image">
-                            <p class="mb-4">{{$project->name}}</p>
-                            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
-                        </div>
-            @endforeach
-            <br>
-            <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Approved Projects</h1>
-            @foreach ($projects as $project)
-                        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                            <img src="{{$project->filepath}}" alt="project image">
-                            <p class="mb-4">{{$project->name}}</p>
-                            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
-                        </div>
-            @endforeach
-            <br>
-            <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Closed Projects</h1>
-            @foreach ($projects as $project)
-                        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                            <img src="{{$project->filepath}}" alt="project image">
-                            <p class="mb-4">{{$project->name}}</p>
-                            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
-                        </div>
-            @endforeach
-            <br>
-            <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">Denied Projects</h1>
-            @foreach ($projects as $project)
-                    
-                        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-                            <img src="{{$project->filepath}}" alt="project image">
-                            <p class="mb-4">{{$project->name}}</p>
-                            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
-                        </div>
-            @endforeach
-            <br>
-            <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Pending Projects</h1>
+        @foreach ($projects as $project)
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <img src="{{$project->filepath}}" alt="project image">
+            <p class="mb-4">{{$project->name}}</p>
+            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
+        </div>
+        @endforeach
+        <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Published Projects</h1>
+        @foreach ($projects as $project)
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <img src="{{$project->filepath}}" alt="project image">
+            <p class="mb-4">{{$project->name}}</p>
+            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
+        </div>
+        @endforeach
+        <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Approved Projects</h1>
+        @foreach ($projects as $project)
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <img src="{{$project->filepath}}" alt="project image">
+            <p class="mb-4">{{$project->name}}</p>
+            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
+        </div>
+        @endforeach
+        <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Closed Projects</h1>
+        @foreach ($projects as $project)
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <img src="{{$project->filepath}}" alt="project image">
+            <p class="mb-4">{{$project->name}}</p>
+            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
+        </div>
+        @endforeach
+        <br>
+        <h1 class="mb-8 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+            Denied Projects</h1>
+        @foreach ($projects as $project)
+
+        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+            <img src="{{$project->filepath}}" alt="project image">
+            <p class="mb-4">{{$project->name}}</p>
+            <p class="mb-4">Created By: {{ $project->owner->name }}</p>
+        </div>
+        @endforeach
+        <br>
+        <br>
         @endif
 
     </div>
 
 </div> --}}
 
-    </div>
+</div>
 
 
 @endsection
-
