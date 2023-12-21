e1a5930945bd8b0df6b792fca1dc2d527a31c25a
Lucaswillaert <lucas.willaert@student.ehb.be>
2023-12-21 10:53:33 +0100

    Add dropdown menu for spaces in nav-bar component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         require app_path('Breadcrumbs/breadcrumbs.php');
+
+        View::composer('components.nav-bar', function ($view) {
+            $view->with('spaces', Auth::user()->spaces);
+        });
     }
 }
--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -28,7 +28,39 @@
                 @endauth
             </ul>
         @endunless
-        <div class="bg-gray-900 p-4">
+
+
+        <div class="bg-gray-900 p-4 flex flex-col">
+
+            <!-- Dropdown menu voor spaces -->
+            <div class="relative inline-block text-left mb-10">
+                <div>
+                    <button type="button"
+                        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                        id="options-menu" aria-haspopup="true" aria-expanded="true">
+                        <!-- Heroicon name: chevron-down -->
+                        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                            fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            
+                <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                        <select onchange="window.location.href=this.value;">
+                            @foreach ($spaces as $space)
+                                <option value="{{ route('spaces.show', $space->id) }}">
+                                    {{ $space->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+            </div>
             @Auth
                 <div class="flex items-center ">
                     <div class="user-image w-10 h-10 rounded-full bg-white"></div>
