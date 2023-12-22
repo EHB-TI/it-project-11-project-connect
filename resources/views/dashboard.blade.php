8cea37b4084756b33af6bd1e7d8f0148714c1190
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-22 11:35:03 +0100

    Add: Student side of dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,221 +1,285 @@
 @extends('components.head')
 @section('title', 'Home')
 @section('content')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
-<div class="flex gap-8">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+    <div class="flex gap-8">
 
-    <div class="w-1/4">
-        @if ($deadline === null)
+        <div class="w-3/4">
+            @if(Auth::check() && Auth::user()->hasRole('student'))
+                <div class="mb-8">
+                    <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">My projects</h2>
+                    @if ($projects->isEmpty())
+                        <p class="mb-4">Nothing here yet, come back later or start your own project.</p>
 
-        @else
-        <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
-            <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
-                Next Deadline</h1>
-            <p class="mb-4">{{$deadline->title}}</p>
-            <p class="mb-4"> <strong>What?</strong> <br> {{$deadline->what}} </p>
-            <p class="mb-4"><strong>When?</strong> <br> {{$deadline->end_date}}</p>
-            <div id="timer"><p id="count"></p></div>
+                    @else
+                        @foreach($projects as $project)
+                        <div class="project-card__container grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+                            <div class="project-card rounded-xl overflow-hidden hover:cursor-pointer"
+                                 onclick="window.location.href='{{ route('projects.show', $project->id) }}'">
+                                <div class="project-card__image bg-cover bg-center w-full h-[200px] bg-amber-500" style="background-image: url( {{ Storage::url($project->file_path) }} )"></div>
+                                <div class="project-card__content rounded-b-xl border-2 border-t-0 p-4 flex flex-col gap-2">
+                                    <h2 class="project-card__title text-2xl font-bold">{{ $project->name }}</h2>
+                                    <p class="project-card__owner">Created By: {{ $project->owner->name }}</p>
+                                    <div class="project-card__members flex items-center gap-4">
+                                        <div class="icon_placeholder w-8 h-8 bg-indigo-950 rounded-full"></div>
+                                        <p>{{ $project->users->count() }} / {{ $project->space->defaultTeamSize }}</p>
+                                    </div>
+                                    <p class="project-card__description">{{ $project->brief }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    @endif
+                </div>
+                <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">Projects I have applied for</h2>
+                @if ($appliedProjects->isEmpty())
+                    <p class="mb-4">Nothing here yet, go ahead and take a look at some projects.</p>
+
+                @else
+                    @foreach($appliedProjects as $project)
+                        <div class="project-card__container grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+                            <div class="project-card rounded-xl overflow-hidden hover:cursor-pointer"
+                                 onclick="window.location.href='{{ route('projects.show', $project->id) }}'">
+                                <div class="project-card__image bg-cover bg-center w-full h-[200px] bg-amber-500" style="background-image: url( {{ Storage::url($project->file_path) }} )"></div>
+                                <div class="project-card__content rounded-b-xl border-2 border-t-0 p-4 flex flex-col gap-2">
+                                    <h2 class="project-card__title text-2xl font-bold">{{ $project->name }}</h2>
+                                    <p class="project-card__owner">Created By: {{ $project->owner->name }}</p>
+                                    <div class="project-card__members flex items-center gap-4">
+                                        <div class="icon_placeholder w-8 h-8 bg-indigo-950 rounded-full"></div>
+                                        <p>{{ $project->users->count() }} / {{ $project->space->defaultTeamSize }}</p>
+                                    </div>
+                                    <p class="project-card__description">{{ $project->brief }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+
+            @endif
+            @if(Auth::check() && Auth::user()->hasRole('teacher'))
+                <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">
+                    Projects</h2>
+                <div class="mb-20">
+                    <canvas id="barChart"></canvas>
+                </div>
+                <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">
+                    Formed Teams</h2>
+                <div>
+                    <canvas id="barChart2"></canvas>
+                </div>
+            @endif
         </div>
-        @endif
+
+        <div class="w-1/4">
+            @if ($deadline === null)
+
+            @else
+                <div class="rounded-xl border-2 overflow-hidden mb-8 p-4">
+                    <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
+                        Next Deadline</h1>
+                    <p class="mb-4">{{$deadline->title}}</p>
+                    <p class="mb-4"><strong>What?</strong> <br> {{$deadline->what}} </p>
+                    <p class="mb-4"><strong>When?</strong> <br> {{$deadline->end_date}}</p>
+                    <div id="timer"><p id="count"></p></div>
+                </div>
+            @endif
+            @if(Auth::check() && Auth::user()->hasRole('teacher'))
+                <div class="">
+                    <canvas id="pieChart" width="400px" height="400px"></canvas>
+                </div>
+            @endif
+        </div>
 
 
     </div>
+    @if($deadline !== null)
+        <script>
+            (function () {
+                //TIMER
+                const divTimer = document.getElementById("timer");
+                const deadlineEndDate = new Date('{{ $deadline->end_date }}');
+                updateTimer(); // Call the function initially
 
-    <div>
-        <canvas id="pieChart" width="400" height="400"></canvas>
-        <canvas id="barChart" width="400" height="400"></canvas>
-        <canvas id="barChart2" width="400" height="400"></canvas>
+                setInterval(updateTimer, 60000);
 
-    </div>
+                function updateTimer() {
+                    const currentDate = new Date();
 
+                    const timeDifference = deadlineEndDate - currentDate;
 
-</div>
-@if($deadline !== null)
+                    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+                    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+                    //const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+                    const timer = document.getElementById('count');
+                    timer.innerHTML = `Time left: ${days} days, ${hours} hours, ${minutes} minutes`;
+                }
+            })();
+
+        </script>
+    @endif
+
+    @if(Auth::check() && Auth::user()->hasRole('teacher'))
     <script>
-        (function() {
-    //TIMER
-    const divTimer = document.getElementById("timer");
-    const deadlineEndDate = new Date('{{ $deadline->end_date }}');
-    updateTimer(); // Call the function initially
 
-    setInterval(updateTimer, 60000);
+        // Pie Chart
+        let labels = ['Product Owner', 'Applicants', 'Inactive Students'];
+        let itemData = [{{$po}}, {{$applicants}}, {{$inactiveStudents}}];
 
-    function updateTimer() {
-        const currentDate = new Date();
+        const data = {
+            labels: labels,
+            datasets: [{
+                data: itemData,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        };
 
-        const timeDifference = deadlineEndDate - currentDate;
+        const pieConfig = {
+            type: 'pie',
+            data: data,
+            options: {
+                responsive: true,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Students'
+                    }
+                }
+            },
+        };
 
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        //const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const pieChart = new Chart(
+            document.getElementById('pieChart'),
+            pieConfig
+        );
 
-        const timer = document.getElementById('count');
-        timer.innerHTML = `Time left: ${days} days, ${hours} hours, ${minutes} minutes`;
-    }
-})();
-        
-    </script>
-@endif
-<script>
-    
-    // Pie Chart
-    let labels = ['Product Owner', 'Applicants', 'Inactive Students'];
-    let itemData = [{{$po}}, {{$applicants}}, {{$inactiveStudents}}];
+        // Bar Chart
+        const barLabels = ['projects'];
+        const barData = {
+            labels: barLabels,
 
-    const data = {
-        labels: labels,
-        datasets: [{
-            data: itemData,
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-        }]
-    };
+            datasets: [{
+                label: 'approved',
+                data: [{{$approvedProjects}}],
+                backgroundColor: 'rgb(75, 192, 192)',
+                borderColor: 'rgb(75, 192, 192)',
+                borderWidth: 1
+            }
+                ,
+                {
+                    label: 'denied',
+                    data: [{{$deniedProjects}}],
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    borderWidth: 1
+                }, {
+                    label: 'closed',
+                    data: [{{$closedProjects}}],
+                    backgroundColor: 'rgb(54, 162, 235)',
+                    borderColor: 'rgb(54, 162, 235)',
+                    borderWidth: 1
+                }, {
+                    label: 'pending',
+                    data: [{{$pendingProjects}}],
+                    backgroundColor: 'rgb(255, 205, 86)',
+                    borderColor: 'rgb(255, 205, 86)',
+                    borderWidth: 1
+                }, {
+                    label: 'published',
+                    data: [{{$publishedProjects}}],
+                    backgroundColor: 'rgb(153, 102, 255)',
+                    borderColor: 'rgb(153, 102, 255)',
+                    borderWidth: 1
+                }
+            ]
+        };
 
-    const pieConfig = {
-        type: 'pie',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Students'
+
+        const barConfig = {
+            type: 'bar',
+            data: barData,
+            options: {
+                responsive: true,
+                indexAxis: 'y',
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Projects Status'
+                    }
+                },
+                scales: {
+                    x: {
+                        max: {{$allProjects}},
+                        beginAtZero: true
+                    },
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        },
-    };
+        };
 
-    const pieChart = new Chart(
-        document.getElementById('pieChart'),
-        pieConfig
-    );
-
-    // Bar Chart
-    const barLabels = ['projects'];
-    const barData = {
-        labels: barLabels,
-        
-        datasets: [{
-            label: 'approved',
-        data: [{{$approvedProjects}}],
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 1
-    }
-    , 
-    {
-        label: 'denied',
-        data: [{{$deniedProjects}}],
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        borderWidth: 1
-    }, {
-        label: 'closed',
-        data: [{{$closedProjects}}],
-        backgroundColor: 'rgb(54, 162, 235)',
-        borderColor: 'rgb(54, 162, 235)',
-        borderWidth: 1
-    }, {
-        label: 'pending',
-        data: [{{$pendingProjects}}],
-        backgroundColor: 'rgb(255, 205, 86)',
-        borderColor: 'rgb(255, 205, 86)',
-        borderWidth: 1
-    }, {
-        label: 'published',
-        data: [{{$publishedProjects}}],
-        backgroundColor: 'rgb(153, 102, 255)',
-        borderColor: 'rgb(153, 102, 255)',
-        borderWidth: 1
-    }
-        ]
-    };
+        const barChart = new Chart(
+            document.getElementById('barChart'),
+            barConfig
+        );
 
 
-    const barConfig = {
-                            type: 'bar',
-                            data: barData,
-                            options: {
-                                responsive: true,
-                                indexAxis: 'y',
-                                plugins: {
-                                    title: {
-                                        display: true,
-                                        text: 'Projects Status'
-                                    }
-                                },
-                                scales: {
-                                    x: {
-                                        max: {{$allProjects}},
-                                        beginAtZero: true 
-                                        },
-                                    y: {
-                                        beginAtZero: true
-                                    }
-                                }
-                            }
-                       };
+        // Bar Chart 2
+        const barLabels2 = ['members'];
+        const datasets2 = [];
 
-    const barChart = new Chart(
-        document.getElementById('barChart'),
-        barConfig
-    );
+        let members = {!! json_encode($members) !!};
+        for (let projectName in members) {
+            let memberCount = members[projectName];
+            datasets2.push({
+                label: projectName,
+                data: [memberCount],
+                backgroundColor: 'rgb(75, 192, 192)',
+                borderColor: 'rgb(75, 192, 192)',
+                borderWidth: 1
+            });
+        }
+        const barData2 = {
+            labels: barLabels2,
+            datasets: datasets2
+        };
 
+        const barConfig2 = {
+            type: 'bar',
+            data: barData2,
+            options: {
+                responsive: true,
+                indexAxis: 'y',
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Formed Teams'
+                    }
+                },
+                scales: {
+                    x: {
+                        max: 14, //should be the max of members for groups
+                        beginAtZero: true
+                    },
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
 
+        const barChart2 = new Chart(
+            document.getElementById('barChart2'),
+            barConfig2
+        );
 
-     // Bar Chart 2
-    const barLabels2 = ['members'];
-    const datasets2 = [];
-    
-    let members = {!! json_encode($members) !!};
-for(let projectName in members) {
-    let memberCount = members[projectName];
-    datasets2.push({
-        label: projectName,
-        data: [memberCount],
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 1
-    });
-}
-    const barData2 = {
-        labels: barLabels2,
-        datasets:datasets2
-    };
-
-    const barConfig2 = {
-                            type: 'bar',
-                            data: barData2,
-                            options: {
-                                responsive: true,
-                                indexAxis: 'y',
-                                plugins: {
-                                    title: {
-                                        display: true,
-                                        text: 'Formed Teams'
-                                    }
-                                },
-                                scales: {
-                                    x: {
-                                        max: 14, //should be the max of members for groups
-                                        beginAtZero: true 
-                                        },
-                                    y: {
-                                        beginAtZero: true
-                                    }
-                                }
-                            }
-                       };
-
-    const barChart2 = new Chart(
-        document.getElementById('barChart2'),
-        barConfig2
-    );
-    
-</script>
+    </script>
+    @endif
 @endsection
--- resources/views/spaces/index.blade.php
+++ resources/views/spaces/index.blade.php
@@ -1,23 +1,25 @@
 @extends('components.head')
 @section('title', 'Home')
 @section('content')
+    <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">Spaces</h2>
+    <p>Select a space:</p>
 
-@if(Auth::check() && Auth::user()->hasRole('teacher'))
-    <a href="{{ route('spaces.create') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold mb-4 py-2 px-4 rounded">
-        Create New Space
-    </a>
-@endif
+    @if(Auth::check() && Auth::user()->hasRole('teacher'))
+        <a href="{{ route('spaces.create') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold mb-4 py-2 px-4 rounded">
+            Create New Space
+        </a>
+    @endif
 
-<div class="grid grid-cols-3 gap-4">
-    @foreach($spaces as $space)
-        <form action="{{ route('spaces.select') }}" method="POST">
-            @csrf
-            <input name="space_id" value="{{ $space->id }}" class="hidden" />
-            <button type="submit" class="cursor-pointer text-center bg-white border-2 hover:border-yellow-500 text-black font-bold w-full h-16 rounded-lg">
-                {{ $space->name }}
-            </button>
-        </form>
-    @endforeach
-</div>
+    <div class="grid grid-cols-3 gap-4">
+        @foreach($spaces as $space)
+            <form action="{{ route('spaces.select') }}" method="POST">
+                @csrf
+                <input name="space_id" value="{{ $space->id }}" class="hidden" />
+                <button type="submit" class="cursor-pointer text-center bg-white border-2 hover:border-yellow-500 text-black font-bold w-full h-16 rounded-lg">
+                    {{ $space->name }}
+                </button>
+            </form>
+        @endforeach
+    </div>
 
 @endsection
