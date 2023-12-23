cbd1b56155b0a1f0bfe98631b4c030024a00dc50
Lucaswillaert <lucas.willaert@student.ehb.be>
2023-12-23 17:34:10 +0100

    Refactor discussion blade template

--- resources/views/projects/detail-sections/discussion.blade.php
+++ resources/views/projects/detail-sections/discussion.blade.php
@@ -1,7 +1,4 @@
-{{-- @extends('components.head')
-@section('content') --}}
-
-@foreach($discussions as $discussion)
+@foreach($projectDiscussions as $discussion)
     <div class="feedback-card rounded-xl border-2 my-8">
         <div class="feedback-card__header flex justify-between border-b-2 p-4">
             <div class="user-info flex items-center gap-2">
@@ -19,7 +16,7 @@
     </div>
 @endforeach
 
-@if($projectFeedback->isEmpty())
+@if($projectDiscussions->isEmpty())
     <div class="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
         <p class="font-bold">No discussion</p>
         <p>There is no discussion yet. @if(Auth::user()->role == 'teacher') Be the first to give feedback!@endif</p>
@@ -30,7 +27,6 @@
     <form id="discussionForm" action="{{ route('discussions.store', $project->id ) }}" method="POST">
         @csrf
         <div class="feedback-card rounded-xl border-2 my-8">
-
             <div class="feedback-card__body p-4">
                 <label for="message" class="text-2xl">Write a discussion</label>
                 <textarea name="message" id="message" cols="30" rows="10" class="w-full p-2 px-4 mt-4 border-2 rounded-xl"></textarea>
@@ -39,12 +35,12 @@
                 <button id="btn" type="submit" class="px-4 py-2 bg-indigo-950 text-white rounded-xl">Submit message</button>
             </div>
         </div>
-
     </form>
 @endif
-{{-- @endsection --}}
+
 <script>
     document.getElementById('discussionForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-});
+        event.preventDefault(); 
+    });
 </script>
+
