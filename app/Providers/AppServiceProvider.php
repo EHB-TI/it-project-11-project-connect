17b55c4f62680bfb5c297169573b3c50b8b09357
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-23 15:10:13 +0100

    Fix: Authentication and space selection issues on navbar component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         require app_path('Breadcrumbs/breadcrumbs.php');
-
-        View::composer('components.nav-bar', function ($view) {
-            $view->with('spaces', Auth::user()->spaces);
-        });
     }
 }
