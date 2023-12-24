6cf968eb2ee8cb6ff623ce20ecf9174c11fb4abe
Gill Mertens <gill.mertens@student.ehb.be>
2023-12-24 02:28:30 +0100

    Fix: filesystems and .env for google cloud deployment

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,11 @@ return [
             'throw' => false,
         ],
 
+        'tmp' => [
+            'driver' => 'local',
+            'root' => storage_path('framework/tmp'),
+        ],
+
     ],
 
     /*
