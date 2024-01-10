af89b56d95db189e1618f4922603c9056e500c40
Gill Mertens <gill.mertens@student.ehb.be>
2024-01-10 02:45:15 +0100

    Minor frontend fixes

--- resources/views/components/application-box.blade.php
+++ resources/views/components/application-box.blade.php
@@ -1,14 +1,14 @@
 <div class="application-box rounded-xl border-2 overflow-hidden mb-8 p-4">
-    <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">{{ $title }}</h1>
-    <p class="mb-1">{{ $message }}</p>
-    @if($route)
-        <a href="{{ route($route, $projectId) }}" class="project-detail__applyButton block w-fit rounded-full px-4 py-2 border-2">{{ $buttonText }}</a>
-    @endif
+    <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 lg:text-3xl">{{ $title }}</h1>
+    <p class="">{{ $message }}</p>
     @if($status)
-        <div class="bg-gray-300 rounded-lg w-fit py-1 px-2">
+        <div class="bg-gray-300 rounded-lg w-fit py-1 px-2 mt-2">
             {{ $status }}
         </div>
     @endif
-    
-   
+    @if($route)
+        <a href="{{ route($route, $projectId) }}" class="project-detail__applyButton mt-2 block w-fit rounded-full px-4 py-2 border-2">{{ $buttonText }}</a>
+    @endif
+
+
 </div>
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -8,8 +8,8 @@
 
 <h2 class="subtitle mb-4 text-xl font-bold leading-none tracking-tight text-gray-700 md:text-2xl lg:text-3xl">Create project</h2>
 <div class="flex flex-col gap-2">
-    
-    <div class="w-1/2">
+
+    <div class="lg:w-1/2">
         <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Project Information</h1>
         <p class="mb-4">Describe why your idea will help fulfill an existing need. Make sure and explain where there is room for
             new technologies to experiment with (not seen during your previous study). Tell us the reason why you
@@ -21,20 +21,20 @@
             @csrf
             <fieldset>
                 <label for="name" class="block font-semibold">Name your project</label>
-                <input type="text" id="name" name="name" class="w-1/2 border border-gray-300 rounded-md px-3 py-2 mt-2 focus:outline-none focus:border-blue-500">
+                <input type="text" id="name" name="name" class="w-full lg:w-1/2 border border-gray-300 rounded-md px-3 py-2 mt-2 focus:outline-none focus:border-blue-500">
             </fieldset>
             <fieldset>
                 <label for="brief" class="block font-semibold">Brief description:</label>
-                <textarea id="brief" name="brief" rows="7" class="w-1/2 resize-none border border-gray-300 rounded-md px-3 py-2 mt-2 focus:outline-none focus:border-blue-500"> </textarea>
+                <textarea id="brief" name="brief" rows="7" class="w-full lg:w-1/2 resize-none border border-gray-300 rounded-md px-3 py-2 mt-2 focus:outline-none focus:border-blue-500"> </textarea>
             </fieldset>
             <div class="flex flex-col space-y-2">
                 <input type="hidden" id="content" name="description" class="hidden">
                 <label for="editor" class="text-gray-600 font-semibold">Write a detailed description down below</label>
                 <div id="editor" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm"></div>
             </div>
-            <div class="mt-4">
+            <div class="mt-4 w-full">
                 <label for="file" class="block text-sm font-medium text-gray$-700">Upload image:</label>
-                <input type="file" id="file" name="file" class="mt-1 py-2 px-4 border rounded-full border-gray-300">
+                <input type="file" id="file" name="file" class="w-full lg:w-1/2 mt-1 py-2 px-4 border rounded-full border-gray-300">
             </div>
 
             <input type="submit" value="Create Project" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 cursor-pointer w-fit">
